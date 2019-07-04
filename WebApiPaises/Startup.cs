da9f1df7c1e4836eb73c34702305c895351e5ea5
Sebastian Chavez <seba.chavez21@gmail.com>
2019-07-04 02:27:57 -0400

    Primer deploy

---
+++ WebApiPaises/Startup.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using WebApiPaises.Models;
+
+namespace WebApiPaises
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(option =>
+            option.UseInMemoryDatabase("paisDB"));
+
+            //Configuracion Json
+            services.AddMvc().AddJsonOptions(ConfigureJson);
+        }
+
+        private void ConfigureJson(MvcJsonOptions obj)
+        {
+            obj.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ApplicationDbContext context)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+
+            app.UseMvc();
+
+            if (!context.Paises.Any())
+            {
+                context.Paises.AddRange(new List<Pais>()
+                {
+                    new Pais{Name = "Rirris", Provincias = new List<Provincia>{new Provincia{ Name = "Quesos"}}},
+                    new Pais{Name = "Madre", Provincias = new List<Provincia>{new Provincia{ Name = "Punes"}}},
+                    new Pais{Name = "Padre", Provincias = new List<Provincia>{new Provincia{ Name = "Olores"}}},
+
+                });
+
+                context.SaveChanges();
+            }
+
+        }
+    }
+}
